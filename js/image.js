e6f68e8842d105976fc281dc006abeeab902599b
Marinishka <shipovalova2015@yandex.ru>
2020-07-21 17:48:29 +0300

    Исправлены ошибки

--- js/image.js
+++ js/image.js
@@ -7,13 +7,17 @@ window.image = (function () {
   var photoInput = document.querySelector('.ad-form__input');
   var photoPreview = document.querySelector('.ad-form__photo');
 
+  var isMatched = function (file) {
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+    return matches;
+  };
+
   var onAvatarInputChange = function () {
     var fileAvatar = avatarInput.files[0];
-    var fileAvatarName = fileAvatar.name.toLowerCase();
-    var matches = FILE_TYPES.some(function (it) {
-      return fileAvatarName.endsWith(it);
-    });
-    if (matches) {
+    if (isMatched(fileAvatar)) {
       var reader = new FileReader();
       reader.addEventListener('load', function () {
         avatarPreview.src = reader.result;
@@ -24,15 +28,11 @@ window.image = (function () {
 
   var onPhotoInputChange = function () {
     var filePhoto = photoInput.files[0];
-    var filePhotoName = filePhoto.name.toLowerCase();
-    var matches = FILE_TYPES.some(function (it) {
-      return filePhotoName.endsWith(it);
-    });
-    if (matches) {
+    if (isMatched(filePhoto)) {
       var reader = new FileReader();
       reader.addEventListener('load', function () {
         var photoPreviewImg = photoPreview.querySelector('img');
-        if (!photoPreviewImg) {
+        if (photoPreviewImg) {
           photoPreviewImg.src = reader.result;
         } else {
           photoPreviewImg = document.createElement('img');
